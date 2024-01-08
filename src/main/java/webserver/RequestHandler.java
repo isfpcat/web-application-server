c3ded243438f788c3c6e80a539c8a572a73a9a01
8wlgns <8wlgns@naver.com>
2024-01-08 16:23:12 +0900

    Apply HttpRequest class

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -19,6 +19,8 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Strings;
+
 import db.DataBase;
 import model.User;
 import util.HttpRequestUtils;
@@ -29,7 +31,6 @@ import util.Util;
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
     
-    private HashMap<String, String> requestHeaderMap;
     private Socket connection;
     private int statusCode;
     
@@ -41,50 +42,7 @@ public class RequestHandler extends Thread {
     
     public RequestHandler (Socket connectionSocket) {
         this.connection = connectionSocket;
-        this.requestHeaderMap = new HashMap<String, String>();
         this.responseHeaderProperty = new ArrayList<Pair>();
-    }
-    
-    private Map<String, String> parseUserParams(BufferedReader reader) {
-    	String requestBody = "";
-		if (requestHeaderMap.containsKey("Content-Length")) {
-			try {
-				requestBody = util.IOUtils.readData(reader, Integer.parseInt(requestHeaderMap.get("Content-Length")));
-				log.debug("requestBody = " + requestBody);
-			} catch (NumberFormatException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return HttpRequestUtils.parseQueryString(requestBody);
-    }
-
-    private void mappingHttpHeaderTohashMap(BufferedReader reader) {
-    	String line = "";
-    	try {
-    		line = reader.readLine();
-    		if (line == null) {
-    			return;
-    		}
-    		Uri uri = HttpRequestUtils.parseUri(line);
-			requestHeaderMap.put("Method", uri.getMethod());
-			requestHeaderMap.put("Uri", uri.getUri());
-			requestHeaderMap.put("Protocol", uri.getProtocol());
-    		
-			String header = "";
-			while(!"".equals((line = reader.readLine()))){
-				header += line+"\n";
-			}
-			
-			Map<String, String> headerMap = HttpRequestUtils.parseHeaders(header);
-			for (String key : headerMap.keySet()) {
-				requestHeaderMap.put(key, headerMap.get(key));
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
     }
     
     public void run() {
@@ -95,118 +53,86 @@ public class RequestHandler extends Thread {
         	InputStreamReader isr = new InputStreamReader(in);
         	BufferedReader reader = new BufferedReader(isr);
         	
-        	mappingHttpHeaderTohashMap(reader);
+        	HttpRequest request = new HttpRequest(reader);
         	
-        	if (requestHeaderMap.containsKey("Cookie")) {
-        		Map<String, String> cookieProperties = HttpRequestUtils.parseCookies(requestHeaderMap.get("Cookie"));
-        		
-        		if (cookieProperties.containsKey("logined")) {
-        			String loginProperty = cookieProperties.get("logined");
-        			if (Boolean.parseBoolean(loginProperty)) {
-            			loginState = LOGIN.LOGINED;
-            		}
-        		}
-        	}
+    		Map<String, String> cookieProperties = HttpRequestUtils.parseCookies(request.getHeader("Cookie"));
+			String logined = cookieProperties.get("logined");
+			if (!Strings.isNullOrEmpty(logined) && Boolean.parseBoolean(logined)) {
+    			loginState = LOGIN.LOGINED;
+    		}
         	
         	DataOutputStream dos = new DataOutputStream(out);
         	byte[] body = {};
         	statusCode = 200;
         	
-	        if (requestHeaderMap.containsKey("Uri") && requestHeaderMap.containsKey("Method")) {
-		        	String uri = requestHeaderMap.get("Uri");
-		        	String method = requestHeaderMap.get("Method");
+        	String uri = request.getPath();
+        	String method = request.getMethod();
 
-	        		if ("/user/create".equals(uri) && "POST".equals(method)) {
-	        			Map<String, String> userParams = parseUserParams(reader);
-	        			String userId = "";
-	    				String password = "";
-	    				String name = "";
-	    				String email = "";
-	    				
-	    				for(String key : userParams.keySet()) {
-							String value = userParams.get(key);
-							
-							if("userId".equals(key)) {
-								userId = value;
-							} else if("password".equals(key)) {
-								password = value;
-							} else if("name".equals(key)) {
-								name = value;
-							} else if("email".equals(key)) {
-								email = URLDecoder.decode(value, "utf-8");
-							}
+	        if (Strings.isNullOrEmpty(uri) && Strings.isNullOrEmpty(method)) {
+
+        		if ("/user/create".equals(uri) && "POST".equals(method)) {
+        			String userId = request.getParameter("userId");
+    				String password = request.getParameter("password");
+    				String name = request.getParameter("name");
+    				String email = URLDecoder.decode(request.getParameter("email"), "utf-8");
+    				statusCode = 302;
+    				
+    				Collection<String> param = new ArrayList<String>(Arrays.asList(userId, password, name, email));
+    				if (Util.isNullOrEmpty(param)) {
+    					log.debug("Create user fail. Required userId, password, name, email");
+    				} else {
+    					User user = new User(userId, password, name, email);
+						DataBase.addUser(user);
+						log.debug("Create user information = " + user.toString());
+    				}
+					
+    			} else if("/user/login".equals(uri) && "POST".equals(method)) {
+        			String userId = request.getParameter("userId");
+    				String password = request.getParameter("password");
+					statusCode = 302;
+					
+					Collection<String> param = new ArrayList<String>(Arrays.asList(userId, password));
+					if (Util.isNullOrEmpty(param)) {
+    					log.debug("Login fail. Required userId, password");
+    				} else {
+    					User user = DataBase.findUserById(userId);
+						String cookie = "";
+						if (user != null && password.equals(user.getPassword())) {
+							loginState = LOGIN.SUCCESS;
+							cookie = "logined=true";
+							log.debug("Login success.");
+						} else {
+							loginState = LOGIN.FAIL;
+							cookie = "logined=false";
+							log.debug("Login failed.");
 						}
-	    				
-	    				statusCode = 302;
-	    				
-	    				Collection<String> param = new ArrayList<String>(Arrays.asList(userId, password, name, email));
-	    				if (Util.isNullOrEmpty(param)) {
-	    					log.debug("Create user fail. Required userId, password, name, email");
-	    				} else {
-	    					User user = new User(userId, password, name, email);
-							DataBase.addUser(user);
-							log.debug("Create user information = " + user.toString());
-	    				}
-						
-	    			} else if("/user/login".equals(uri) && "POST".equals(method)) {
-	    				Map<String, String> userParams = parseUserParams(reader);
-	        			String userId = "";
-	    				String password = "";
-	    				
-						for(String key : userParams.keySet()) {
-							String value = userParams.get(key);
-							if ("userId".equals(key)) {
-								userId = value;
-							} else if("password".equals(key)) {
-								password = value;
-							} 
-						}
-						statusCode = 302;
-						
-						Collection<String> param = new ArrayList<String>(Arrays.asList(userId, password));
-						if (Util.isNullOrEmpty(param)) {
-	    					log.debug("Login fail. Required userId, password");
-	    				} else {
-	    					User user = DataBase.findUserById(userId);
-							String cookie = "";
-							if (user != null && password.equals(user.getPassword())) {
-								loginState = LOGIN.SUCCESS;
-								cookie = "logined=true";
-								log.debug("Login success.");
-							} else {
-								loginState = LOGIN.FAIL;
-								cookie = "logined=false";
-								log.debug("Login failed.");
-							}
-							responseHeaderProperty.add(new Pair("Set-Cookie", cookie));
-	    				}
-						
-	    			} else if("/user/list".equals(uri) && "GET".equals(method)) {
-	    				if (loginState == LOGIN.LOGINED) {
-	    					StringBuilder userListHtml = new StringBuilder();
-	    					Collection<User> usrList = DataBase.findAll();
-	    					for (User user : usrList) {
-	    						String name = user.getName();
-	    						String email = user.getEmail();
-	    						userListHtml.append(name + " " + email + "\n");
-	    					}
-	    					
-	    					body = userListHtml.toString().getBytes();
-	    				} else {
-	    					statusCode = 302;
-	    				  	log.debug("Redirect to login.html ");
-	    				}
-	    			} else {
-	    				body = Files.readAllBytes(new File("./webapp" + ("/".equals(uri) ? "/index.html" : uri)).toPath());
-	    			}
+						responseHeaderProperty.add(new Pair("Set-Cookie", cookie));
+    				}
+					
+    			} else if("/user/list".equals(uri) && "GET".equals(method)) {
+    				if (loginState == LOGIN.LOGINED) {
+    					StringBuilder userListHtml = new StringBuilder();
+    					Collection<User> usrList = DataBase.findAll();
+    					for (User user : usrList) {
+    						String name = user.getName();
+    						String email = user.getEmail();
+    						userListHtml.append(name + " " + email + "\n");
+    					}
+    					
+    					body = userListHtml.toString().getBytes();
+    				} else {
+    					statusCode = 302;
+    				  	log.debug("Redirect to login.html ");
+    				}
+    			} else {
+    				body = Files.readAllBytes(new File("./webapp" + ("/".equals(uri) ? "/index.html" : uri)).toPath());
+    			}
         	} else {
         		body = Files.readAllBytes(new File("./webapp/404.html").toPath());
         	}
 	        
-	        if (requestHeaderMap.containsKey("Accept")) {
-	        	String accept = requestHeaderMap.get("Accept").split(",")[0];
-	        	responseHeaderProperty.add(new Pair("Content-Type", accept));
-	        }
+        	String accept = request.getHeader("Accept");
+        	responseHeaderProperty.add(new Pair("Content-Type", accept));
 	        
 	        if (body.length > 0) {
 	        	responseHeaderProperty.add(new Pair("Content-Length", String.valueOf(body.length)));
